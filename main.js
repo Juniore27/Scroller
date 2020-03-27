0106b9e5728e5e8b4bf99b6054eb2fe57ae79c9f
Juniore <babanarowerze5@wp.pl>
2020-03-27 13:05:40 +0100

    Add support for keyboard

--- main.js
+++ main.js
@@ -59,3 +59,17 @@ document.addEventListener('wheel', (event) => {
 //  document.addEventListener('mousewheel', scroller.listenScroll)   //SKROCONA WERSJA ZAPISU POWYZEJ
 document.addEventListener('swipeUp', () => scroller.scroll(1))
 document.addEventListener('swipeDown', () => scroller.scroll(-1))
+document.addEventListener('keydown', (e) => {
+
+  switch (e.keyCode) {
+    case 40:
+      return scroller.scroll(1)
+      break;
+    case 38:
+      return scroller.scroll(-1)
+      break;
+
+    default:
+      break;
+  }
+})
